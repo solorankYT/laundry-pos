b5522f38303071677ed488f67f0793336e4586d7
solorankYT <43119617+solorankYT@users.noreply.github.com>
2026-04-08 01:51:38 +0800

    app login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/useAuth'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Orders from './pages/Orders'
 import Dashboard from './pages/Dashboard'
 
