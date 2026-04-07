5a1cafba4d65b5105de41866a0c9f6e25064cf9f
solorankYT <43119617+solorankYT@users.noreply.github.com>
2026-04-08 02:11:53 +0800

    dashboard added button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 
 export default function Dashboard(){
 return(
-    <div>hello</div>
+    <div>
+    <a href="/orders" className="text-blue-500 hover:underline">Go to Orders</a>
+    </div>
 )
     
 }
